06cb7546eeecfd9f27f9fbedb98dd1bea7e558ce
Atharva-007 <84835584+Atharva-007@users.noreply.github.com>
2025-06-25 17:45:29 +0530

    updated index

--- api/index.py
+++ api/index.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, send_from_directory
 import os
 import json
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 # Load resume data
 with open(os.path.join(os.path.dirname(__file__), '../data/resume_data.json')) as f:
     resume_data = json.load(f)
 
 @app.route('/')
-def resume():
+def home():
     return render_template('resume.html', data=resume_data)
 
 @app.route('/static/<path:filename>')
@@ -17,6 +17,5 @@ def serve_static(filename):
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     return send_from_directory(os.path.join(root_dir, 'static'), filename)
 
-# Vercel requirement
 if __name__ == '__main__':
     app.run(debug=True)
